ab2c910d3364e72dd0fb9969f3704a30b5cc0cad
kokokocococo555 <kokokocococo555@gmail.com>
2019-06-08 14:26:43 +0900

    チュートリアルその4完了
    - フォームの作成
    - 汎用ビューの使用

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -10,12 +10,15 @@ urlpatterns = [
     # nameの値はtemplateで{% url %}で呼ばれる
     # urlのパスを変更したいとき、template内での変更が不要になる
     # urls.pyのpath()で変更すれば良い
+
+    # 汎用ビューを使用
+
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,34 +3,37 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # テンプレート変数名をPythonオブジェクトにマッピングする辞書
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # template = loader.get_template('polls/index.html')
-    # return HttpResponse(template.render(context, request))
-    # render()を使って省略して書ける
-    # render(request, テンプレート, コンテキスト)という順に引数に入れる
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+# def detail(request, question_id):
     # question = get_object_or_404(Question, pk=question_id)
     # return render(request, 'polls/detail.html', {'question': question})
+
     # よくある処理なので「汎用ビュー(generic view)」が用意されている(https://docs.djangoproject.com/ja/2.2/intro/tutorial04/)
     # 1. URLを介してパラメータが渡される
     # 2. -> パラメータに従ってデータベースからデータを取り出す
     # 3. -> テンプレートをロードする
     # 4. -> レンダリングしたテンプレートを返す 
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     # 複数人のユーザーが同時に投票しようとするケースにバグがある
