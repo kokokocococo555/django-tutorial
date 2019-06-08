f153dcec3b83f0037ca0dabf73360658887f0633
kokokocococo555 <kokokocococo555@gmail.com>
2019-06-08 15:54:05 +0900

    チュートリアルその5完了
    Djangoの自動テスト実装（一部機能のみ）

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -13,7 +14,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -28,6 +31,9 @@ class DetailView(generic.DetailView):
     # 4. -> レンダリングしたテンプレートを返す 
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
